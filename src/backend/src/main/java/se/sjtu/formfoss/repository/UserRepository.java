3b761f873e33288a95300992ac0ffc445d0d2e21
aceforeverd <aceforeverd@users.noreply.github.com>
2017-06-28 20:40:04 -0500

    Refactor (#23)
    * generic editorconfig
    
    * ER diagram (#22)
    
    * structure  optimization (#20)
    
    * enhancement
    
    * ER
    
    ER
    
    * the description
    
    * fix syntax bug
    
    * Refactoring the project
    
    * pom.xml enhancement
    
    * enhance

---
+++ src/backend/src/main/java/se/sjtu/formfoss/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package se.sjtu.formfoss.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import se.sjtu.formfoss.model.UserEntity;
+
+import java.util.List;
+/**
+ * Created by ace on 6/28/17.
+ */
+public interface UserRepository extends CrudRepository<UserEntity, Integer> {
+    List<UserEntity> findByUserNameContainingIgnoreCase(String userName);
+    List<UserEntity> findByUserEmailContainingIgnoreCase(String userEmail);
+    List<UserEntity> findByUserPhoneContainingIgnoreCase(String userPhone);
+}
