52573d511b24dc3458a1c4ccae1e41dfea6c8d1a
aceforeverd <dace1p@gmail.com>
2017-07-07 22:58:30 +0800

    remove unexpected change in user.service

--- src/frontend/src/main/frontend/src/app/services/user.service.ts
+++ src/frontend/src/main/frontend/src/app/services/user.service.ts
@@ -9,7 +9,7 @@ import { User } from '../models/user';
 export class UserService {
     private userUrl = '/users/';
     private jsonHeader = new Headers({'Content-Type': 'application/json'});
-    private formHeader = new Headers({"Content-Type": "application/x-www-formGroup-urlencoded"});
+    private formHeader = new Headers({"Content-Type": "application/x-www-form-urlencoded"});
 
     constructor(private http: Http) { }
 
