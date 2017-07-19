0f1ff4abbe684d1f2d2798ccf02fdc7544d825ce
sunjiahao <826371443@qq.com>
2017-07-19 17:38:01 +0800

    user edit & detail

--- src/frontend/src/main/frontend/src/app/component/users/user-edit/user-edit.component.ts
+++ src/frontend/src/main/frontend/src/app/component/users/user-edit/user-edit.component.ts
@@ -1,47 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../../models/user';
-import {UserService} from '../../../services/user.service';
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {UserService} from "../../../services/user.service";
+import {ActivatedRoute, ParamMap} from "@angular/router";
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
-import {AlertService} from '../../../services/alert.service';
-
 
 @Component({
     selector: 'app-user-edit',
-    templateUrl: './user-edit.component.html',
+    templateUrl: './user-eidt.component.html',
     styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
     user: User;
 
     constructor(private userService: UserService,
-                private activatedRoute: ActivatedRoute,
-                private router: Router,
-                private location: Location,
-                private alertService: AlertService) { }
+                private route: ActivatedRoute,
+                private location: Location) { }
 
     save(): void {
         this.userService.update(this.user)
-            .subscribe(
-                (res: string) => {
-                    this.router.navigate(['/user', this.user.userId])
-                        .then(() => this.alertService.success(res['message'] || JSON.stringify(res)));
-                },
-                (error: string) => this.alertService.error(error));
+            .subscribe((user: User) => this.user = user)
     }
 
-    goBack(): void {
+    goBack() : void {
         this.location.back();
     }
 
     ngOnInit() {
-        this.activatedRoute.paramMap
+        this.route.paramMap
             .switchMap((params: ParamMap) => {
                 return this.userService.getUser(+params.get('id'))
             })
-            .subscribe(user => this.user = user,
-                error => this.alertService.error(error));
+            .subscribe(user => this.user = user);
     }
 
 
