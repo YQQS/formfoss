297bd10eef22f62dc7954313c5deecee6f1994de
sunjiahao <826371443@qq.com>
2017-07-21 11:12:06 +0800

    edit and detail repair

--- src/frontend/src/main/frontend/src/app/component/users/user-edit/user-edit.component.ts
+++ src/frontend/src/main/frontend/src/app/component/users/user-edit/user-edit.component.ts
@@ -7,7 +7,7 @@ import 'rxjs/add/operator/switchMap';
 
 @Component({
     selector: 'app-user-edit',
-    templateUrl: './user-edit.component.html',
+    templateUrl: './user-eidt.component.html',
     styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
