7fbe90c0b29d523f058f13a43789515891ee34a3
sunjiahao <826371443@qq.com>
2017-07-21 13:22:04 +0800

    complete edit and details of admin

--- src/frontend/src/main/frontend/src/app/app-routing.module.ts
+++ src/frontend/src/main/frontend/src/app/app-routing.module.ts
@@ -3,51 +3,36 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './component/users/login/login.component';
 import { RegisterComponent } from './component/users/register/register.component';
-import { UserListComponent } from './component/users/user-list/user-list.component';
-import {UserDetailComponent} from './component/users/user-detail/user-detail.component';
-import {FormPrototypeComponent} from './component/form-prototype/form-prototype.component';
-import {FormListComponent} from './component/form/list/form-list.component';
-import {FormPreviewComponent} from './component/form/view/form-preview/form-preview.component';
-import {FormEditComponent} from './component/form/edit/form-edit/form-edit.component';
-import {FormNewComponent} from './component/form/edit/form-new/form-new.component';
-import {FormStatComponent} from './component/form/stat/form-stat/form-stat.component';
-import {UserStatComponent} from './component/form/stat/user-stat/user-stat.component';
-import {HomeComponent} from './component/extra/home/home.component';
-import {AuthGuard} from './component/_guards/auth.guard';
-import {PageNotFoundComponent} from './component/extra/page-not-found/page-not-found.component';
-import {CanNotAccessComponent} from './component/extra/can-not-access/can-not-access.component';
-import {AdminGuard} from './component/_guards/admin.guard';
-import {UserEditComponent} from './component/users/user-edit/user-edit.component';
+import { UsersComponent } from './component/users/users.component';
+import {UserDetailComponent} from "./component/users/user-detail/user-detail.component";
+import {FormPrototypeComponent} from "./component/form-prototype/form-prototype.component";
+import {DynamicFormComponent} from "./component/questions/dynamic-form/dynamic-form.component";
+import {DynamicEditComponent} from "./component/questions/dynamic-edit/dynamic-edit.component";
+import {QuestionListComponent} from './component/questions/question-list/question-list.component';
+import {FormPreviewComponent} from './component/questions/dynamic-form/form-preview/form-preview.component';
+import {FormEditComponent} from './component/questions/dynamic-edit/form-edit/form-edit.component';
+import {FormNewComponent} from './component/questions/dynamic-edit/form-new/form-new.component';
+import {FRQuestionComponent} from "./component/questions/form-result/frquestion/frquestion.component";
+import {FRStatComponent} from "./component/questions/form-result/frstat/frstat.component";
+import {FRUserComponent} from "./component/questions/form-result/fruser/fruser.component";
+import {HomePageComponent} from "./component/questions/homepage/homepage.component";
+import {UserEditComponent} from "./component/users/user-edit/user-edit.component";
 
-const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
-    {path: 'prototype', component: FormPrototypeComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
-
-    {path: 'user', canActivate: [AuthGuard], children: [
-        {path: 'list', component: UserListComponent, canActivate: [AdminGuard]},
-        {path: ':id', children: [
-            {path: '', component: UserDetailComponent},
-            {path: 'edit', component: UserEditComponent},
-        ]},
-    ]},
-    {path: 'question', children: [
-        {path: 'new', component: FormNewComponent, canActivate: [AuthGuard]},
-        {path: 'list', component: FormListComponent, canActivate: [AuthGuard]},
-        {path: ':id', children: [
-            {path: '', redirectTo: 'view', pathMatch: 'prefix'},
-            {path: 'view', component: FormPreviewComponent},
-            {path: 'edit', component: FormEditComponent, canActivate: [AuthGuard]},
-            {path: 'stat', canActivate: [AuthGuard], children: [
-                {path: '', component: FormStatComponent},
-                {path: 'user', component: UserStatComponent}
-            ]}
-        ]}
-    ]},
-    {path: 'can-not-access', component: CanNotAccessComponent},
-    {path: '**', component: PageNotFoundComponent}
+const routes:Routes = [
+    {path: "", redirectTo: '/homepage', pathMatch: 'full'},
+    {path: "homepage" ,component:HomePageComponent},
+    {path: "prototype", component: FormPrototypeComponent},
+    {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent},
+    {path: "list", component: UsersComponent},
+    {path: "editusers/:id", component: UserEditComponent},
+    {path: "users/:id", component: UserDetailComponent},
+    {path: 'questionList', component: QuestionListComponent},
+    {path: 'questions/edit/:id', component: FormEditComponent},
+    {path: 'questions/new', component: FormNewComponent},
+    {path: 'questions/:id', component: FormPreviewComponent},
+    {path: 'formStat/:id', component: FRStatComponent},
+    {path: 'formUserStat/:id', component: FRUserComponent}
 ];
 
 @NgModule({
--- src/frontend/src/main/frontend/src/app/app.module.ts
+++ src/frontend/src/main/frontend/src/app/app.module.ts
@@ -8,7 +8,7 @@ import {RegisterComponent} from './component/users/register/register.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {AppRoutingModule} from './app-routing.module';
-import {UserListComponent} from './component/users/user-list/user-list.component';
+import {UsersComponent} from './component/users/users.component';
 import { UserDetailComponent } from './component/users/user-detail/user-detail.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {
@@ -28,62 +28,46 @@ import {
 } from '@angular/material';
 import 'hammerjs';
 import { FormPrototypeComponent } from './component/form-prototype/form-prototype.component';
-import { FormViewComponent } from './component/form/view/form-view/form-view.component';
-import { QuestionViewComponent } from './component/form/view/question-view/question-view.component';
-import { FormStructureEditComponent } from './component/form/edit/form-structure-edit/form-structure-edit.component';
-import {QuestionService} from './services/question.service';
-import { HomeComponent} from './component/extra/home/home.component';
-import { FormListComponent } from './component/form/list/form-list.component';
-import { FormPreviewComponent } from './component/form/view/form-preview/form-preview.component';
-import { FormEditComponent } from './component/form/edit/form-edit/form-edit.component';
-import { FormNewComponent } from './component/form/edit/form-new/form-new.component';
-import { FormSettingsComponent } from './component/form/edit/form-settings/form-settings.component';
-import { QuestionStatComponent } from './component/form/stat/question-stat/question-stat.component';
-import { UserStatComponent } from './component/form/stat/user-stat/user-stat.component';
-import { FormStatComponent } from './component/form/stat/form-stat/form-stat.component';
-import {ChartsModule} from 'ng2-charts';
-import {APP_BASE_HREF, HashLocationStrategy, LocationStrategy} from '@angular/common';
-import {SubmitPreviewComponent} from './component/form/view/form-submit-preview/submit-preview.component';
-import {UserEditComponent} from './component/users/user-edit/user-edit.component';
-import { AlertDialogComponent } from './component/_directives/alert-dialog/alert-dialog.component';
-import {AlertService} from './services/alert.service';
-import {AlertComponent} from './component/_directives/alert/alert.component';
-import {AuthGuard} from './component/_guards/auth.guard';
-import { PageNotFoundComponent } from './component/extra/page-not-found/page-not-found.component';
-import { CanNotAccessComponent } from './component/extra/can-not-access/can-not-access.component';
-import {AdminGuard} from './component/_guards/admin.guard';
-import { AdminComponent } from './component/_navigation/admin/admin.component';
-import { ToolbarComponent } from './component/_navigation/toolbar/toolbar.component';
-
+import { DynamicFormComponent } from './component/questions/dynamic-form/dynamic-form.component';
+import { DynamicQuestionComponent } from './component/questions/dynamic-question/dynamic-question.component';
+import { DynamicEditComponent } from './component/questions/dynamic-edit/dynamic-edit.component';
+import {QuestionService} from "./services/question.service";
+import { HomePageComponent} from './component/questions/homepage/homepage.component';
+import { QuestionListComponent } from './component/questions/question-list/question-list.component';
+import { FormPreviewComponent } from './component/questions/dynamic-form/form-preview/form-preview.component';
+import { FormEditComponent } from './component/questions/dynamic-edit/form-edit/form-edit.component';
+import { FormNewComponent } from './component/questions/dynamic-edit/form-new/form-new.component';
+import { FormSettingsComponent } from './component/questions/dynamic-edit/form-settings/form-settings.component';
+import { FRQuestionComponent } from './component/questions/form-result/frquestion/frquestion.component';
+import { FRUserComponent } from './component/questions/form-result/fruser/fruser.component';
+import { FRStatComponent } from './component/questions/form-result/frstat/frstat.component';
+import {ChartsModule} from "ng2-charts";
+import {HashLocationStrategy, LocationStrategy} from "@angular/common";
+import {SubmitPreviewComponent} from "./component/questions/dynamic-form/submit-preview/submit-preview.component";
+import {UserEditComponent} from "./component/users/user-edit/user-edit.component";
 @NgModule({
     declarations: [
         AppComponent,
         LoginComponent,
         RegisterComponent,
-        UserListComponent,
+        UsersComponent,
         UserDetailComponent,
         FormPrototypeComponent,
-        FormViewComponent,
-        QuestionViewComponent,
-        FormStructureEditComponent,
-        FormListComponent,
-        FormListComponent,
+        DynamicFormComponent,
+        DynamicQuestionComponent,
+        DynamicEditComponent,
+        QuestionListComponent,
+        QuestionListComponent,
         FormPreviewComponent,
         FormEditComponent,
         FormNewComponent,
         FormSettingsComponent,
-        QuestionStatComponent,
-        UserStatComponent,
-        FormStatComponent,
-        HomeComponent,
+        FRQuestionComponent,
+        FRUserComponent,
+        FRStatComponent,
+        HomePageComponent,
         SubmitPreviewComponent,
-        AlertDialogComponent,
-        UserEditComponent,
-        AlertComponent,
-        PageNotFoundComponent,
-        CanNotAccessComponent,
-        AdminComponent,
-        ToolbarComponent
+        UserEditComponent
     ],
     imports: [
         BrowserModule,
@@ -120,11 +104,8 @@ import { ToolbarComponent } from './component/_navigation/toolbar/toolbar.compon
         MdSnackBarModule,
         ChartsModule
     ],
-    entryComponents: [SubmitPreviewComponent, AlertDialogComponent],
-    providers: [UserService, QuestionService, AlertService, AuthGuard, AdminGuard, {
-        provide: LocationStrategy, useClass: HashLocationStrategy}, {
-        provide: APP_BASE_HREF, useValue: '/'
-    }],
+    entryComponents: [SubmitPreviewComponent],
+    providers: [UserService, QuestionService ,{provide: LocationStrategy, useClass: HashLocationStrategy}],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
