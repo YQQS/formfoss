98c1ad0e47d7b815763e3e04c10ad2c07862ef2c
sunjiahao <826371443@qq.com>
2017-07-21 11:44:49 +0800

    detail and edit repair

--- src/frontend/src/main/frontend/src/app/services/user.service.ts
+++ src/frontend/src/main/frontend/src/app/services/user.service.ts
@@ -6,6 +6,10 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/delay'
+
+import 'rxjs/add/operator/debounceTime'
+import 'rxjs/add/operator/distinctUntilChanged'
+
 import {Observable} from 'rxjs/Observable';
 import { User } from '../models/user';
 
