b757d9b8e41369c91261923f183519b75360e8ec
aceforeverd <aceforever@protonmail.com>
2017-08-26 20:33:11 +0800

    feature improve

--- src/backend/src/main/java/se/sjtu/formfoss/controller/PublicController.java
+++ src/backend/src/main/java/se/sjtu/formfoss/controller/PublicController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import se.sjtu.formfoss.model.FormEntity;
 import se.sjtu.formfoss.service.FormService;
@@ -18,7 +19,7 @@ public class PublicController {
     @Autowired
     private FormService formService;
 
-    @RequestMapping(path = "/forms/published")
+    @RequestMapping(path = "/forms/published", method = RequestMethod.GET)
     public @ResponseBody
     ResponseEntity<List<FormEntity>> getPublishedForm() {
         return new ResponseEntity<>(
