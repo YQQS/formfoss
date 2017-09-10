5bdcdb939a1bbac14a74a46915a463acca2567c2
sunjiahao <826371443@qq.com>
2017-09-10 16:36:59 +0800

    user self edit and delete

--- src/frontend/src/main/frontend/src/app/component/users/user-self-edit/user-self-edit.component.ts
+++ src/frontend/src/main/frontend/src/app/component/users/user-self-edit/user-self-edit.component.ts
@@ -11,7 +11,7 @@ import {AlertService} from '../../../services/alert.service';
 @Component({
     selector: 'app-user-self-edit',
     templateUrl: './user-self-edit.component.html',
-    styleUrls: ['./user-self-edit.component.scss']
+    styleUrls: ['./user-self-edit.component.css']
 })
 export class UserSelfEditComponent implements OnInit {
     user: User;
